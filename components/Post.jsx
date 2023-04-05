afbb2d47fe2cdc1198fa04899335528d5064adc8
Aaron Latham <alatham7891@gmail.com>
2023-04-05 11:23:41 +0100

    fixed post avatar

--- components/Post.jsx
+++ components/Post.jsx
@@ -7,10 +7,7 @@ function Post({ post }) {
                 <div className="flex gap-3 mb-3">
                     <div className="avatar">
                         <div className="w-14 rounded-full border-2 ring-offset-base-100 ring-offset-2">
-                            <img
-                                src="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-                                alt="avatar"
-                            />
+                            <img src={post.avatar} alt="avatar" />
                         </div>
                     </div>
                     <div>
